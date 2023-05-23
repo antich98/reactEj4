679ab7690db00ceac403a7d2b13fc33e16623eab
Santiago Antich Llarrull <santiantich98@gmail.com>
2023-05-23 11:39:29 -0300

    Comencé la lógica para crear la tarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,14 +1,24 @@
 import { Form, Button } from "react-bootstrap";
-
-import React from 'react';
+import React, { useState } from 'react';
 import ListaTareas from "./ListaTareas";
 
 const formularioTarea = () => {
+    const [tarea, setTarea] = useState('');
+    const [tareas, setTareas] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTareas([...tarea, tarea]);
+        setTarea('');
+    }
     return (
         <>
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3 d-flex" controlId="tarea">
-                <Form.Control type="text" placeholder="Ingrese una tarea" />
+                <Form.Control 
+                type="text" 
+                placeholder="Ingrese una tarea" 
+                onChange={(e) => setTarea(e.target.value)} value={tarea} />
                 <Button variant="primary">Agregar tarea</Button>
             </Form.Group>
         </Form>
