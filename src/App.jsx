97bd92906ec3eb29e67b77c406838230a9c80a05
Santiago Antich Llarrull <santiantich98@gmail.com>
2023-05-23 10:37:53 -0300

    Terminé el título, footer y background

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
-import Button from 'react-bootstrap/Button';
+import { Container } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-    
+      <Container className="my-5 main">
+        <section className="text-light">
+          <h1 className="text-center display-4">Lista de tareas</h1>
+          <hr />
+        </section>
+      </Container>
+      <footer className="bg-dark text-center text-light py-4">
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
